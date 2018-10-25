37630d946cc752fc1de6e61dd495a40890561159
bordway <bordway@ihmc.us>
2018-10-25 17:06:26 -0500

    Updated the grpc functions

--- python/text_topic_generator/text_topic_generator.py
+++ python/text_topic_generator/text_topic_generator.py
@@ -5,14 +5,23 @@ from . import topic_gen_model as model
 class TopicGenerator(gen_grpc.TopicGenerationServicer):
     def __init__(self):
         self.model = model.TopicModelling()
-        pass
 
     def GenerateTopicsDirect(self, loc, context):
-        with open(loc, 'r') as document:
-            topics = self.model.generate_topics(document)
-
+        topics = self.generate(loc)
         return gen.Topics(topics=topics)
 
     def GenerateTopicsIndirect(self, loc, context):
-        return gen.FileLocation(location="~/")
+        topics = self.generate(loc)
 
+        # Open a file and write the topics there
+        topic_loc = loc + '-topics'
+        with open(topic_loc, 'w') as document:
+            document.write(topics)
+
+        return gen.FileLocation(location=topic_loc)
+
+    def generate(self, loc):
+        with open(loc, 'r') as document:
+            topics = self.model.generate_topics(document)
+
+        return topics
